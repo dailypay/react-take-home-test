9ff7db786ce5ffd7ff6ee5537428706336f9d4ce
Rafael Freaner <rafaelfreaner@R-Freaners-MBP.fios-router.home>
2021-01-06 09:52:46 -0500

    add ballot shell component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
+import Ballot from './Components/Ballot/Ballot';
 
 function App() {
+  // Feel free to remove the contents of the header tag to make more room for your code
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
       </header>
+      <Ballot />
     </div>
   );
 }
---
+++ src/Components/Ballot/Ballot.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Ballot = () => {
+  return (
+    <div className='ballot'>
+      'Your Code Goes Here'
+    </div>
+  )
+}
+
+export default Ballot;
